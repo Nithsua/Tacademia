9b6c02f10fb7908e60fcb50d739633e0bebdba6c
Nivas Muthu M G <nivasmuthumg@icloud.com>
2021-07-03 11:51:03 +0530

    Initial Commit

---
+++ Tacademia/TacademiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TacademiaApp.swift
+//  Tacademia
+//
+//  Created by Nivas Muthu M G on 03/07/21.
+//
+
+import SwiftUI
+
+@main
+struct TacademiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
